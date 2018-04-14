e8517e6e39e7054b46b4770561179bd746bcdefc
Matthieu Bonnevalle <mat.bonnevalle@gmail.com>
2018-04-14 14:09:23 +0200

    Start lo54 project

---
+++ src/main/java/fr/utbm/lo54/controller/DefaultClientController.java
@@ -0,0 +1,11 @@
+package fr.utbm.lo54.controller;
+
+/**
+ * Created by matbo on 14/04/2018.
+ */
+public class DefaultClientController {
+
+    public void registerClientFromInput(){
+        //todo scanner des informations client
+    }
+}
---
+++ src/main/java/fr/utbm/lo54/service/ClientService.java
@@ -0,0 +1,12 @@
+package fr.utbm.lo54.service;
+
+import fr.utbm.lo54.entity.Client;
+
+/**
+ * Created by matbo on 14/04/2018.
+ */
+public class ClientService {
+    public void registerClient(Client client){
+        //todo appelle vers hibernateDao pour enregistrer dans la bdd
+    }
+}
